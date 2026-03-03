fbe84de79247c1aa3932b6801b282729a5a80a78
Bianca <dancibianca1@gmail.com>
2026-03-03 15:42:31 +0200

    Created class

---
+++ NotepadPlusPlus/Models/Page.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotepadPlusPlus.Models
+{
+    internal class Page
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime LastModified { get; set; }
+
+        public Page()
+        {
+            Title = "Untitled";
+            Content = string.Empty;
+            LastModified = DateTime.Now;
+        }
+
+        public Page(string title, string content)
+        {
+            Title = title;
+            Content = content;
+            LastModified = DateTime.Now;
+        }
+
+        public void UpdateContent(string newContent)
+        {
+            Content = newContent;
+            LastModified = DateTime.Now;
+        }
+    }
+}
